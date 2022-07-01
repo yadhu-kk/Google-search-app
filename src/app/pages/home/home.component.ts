d23eda81137d3dacf4cb460580e2cb4d9b2d01b8
yadhukk <yadhukky@gmail.com>
2022-07-01 15:47:51 +0530

    tried interface

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SearchService } from 'src/app/services/search.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  searchItem = '';
+  Resultmodel: any;
 
-  constructor() { }
+  constructor(private http: SearchService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit() {
+    this.Resultmodel = this.http.search(this.searchItem);
   }
-
 }
---
+++ src/app/services/search.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Resultmodel } from '../models/resultmodel';
+
+const mockResults: Resultmodel[] = [
+  {
+    url: 'https://angular.io/',
+    title: 'angular',
+    description:
+      'Angular is a platform for building mobile and desktop web applications. Join the community of millions of developers who build compelling user interfaces',
+    date: new Date('2022-02-01'),
+    links: ['https://angular.io/'],
+  },
+  {
+    url: 'https://www.w3schools.com/js/',
+    title: 'JavaScript Tutorial - W3Schools',
+    description:
+      'JavaScript is the programming language of the Web. JavaScript is easy to learn',
+    date: new Date('2022 - 03 - 04'),
+    links: ['https://www.w3schools.com/js/'],
+  },
+];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchService {
+  constructor() {}
+  search(query: string): Resultmodel[] {
+    return mockResults.filter((item) => {
+      return item.title.includes(query);
+    });
+  }
+}
